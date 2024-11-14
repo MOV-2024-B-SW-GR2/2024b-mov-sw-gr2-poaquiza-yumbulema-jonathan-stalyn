563e5de0d77e9c31897a8c1901ac2eacf8a507b5
jonathanpoaquiza <94476415+jonathanpoaquiza@users.noreply.github.com>
2024-11-13 23:33:52 -0500

    Add tema arreglos

--- 00-Kotlin/kotlin-ejemplo/src/main/kotlin/Main.kt
+++ 00-Kotlin/kotlin-ejemplo/src/main/kotlin/Main.kt
@@ -45,9 +45,96 @@ fun main(args: Array<String>) {
     calcularSueldo(sueldo=10.00,bonoEspecial=20.00)// bono en 2do lugar
     //gracias a los parametros nombrados
     calcularSueldo(bonoEspecial=20.00,sueldo=10.00, tasa = 14.00)// bono en 1er lugar
+
+    val sumaA=Suma(1,1)
+    val sumaB=Suma(null,1)
+    val sumaC=Suma(1,null)
+    val sumaD=Suma(null,null)
+    sumaA.sumar()
+    sumaB.sumar()
+    sumaC.sumar()
+    sumaD.sumar()
+    println(Suma.elevarAlCuadrado(2))
+    println(Suma.historialSumas)
+
+    //Arreglos
+    //estaticos: "arrayOf" que es estatico (no puede cambiar los elementos)
+    val arregloEstatico: Array<Int> = arrayOf<Int>(1,2,3)
+    println(arregloEstatico)
+    //dinamicos: "arrayListOf" que puede cambiar los elementos
+    val arregloDinamico: ArrayList<Int> = arrayListOf<Int>(
+        1,2,3,4,5,6,7,8,9,10
+    )
+    print(arregloDinamico)
+    arregloDinamico.add(11)
+    arregloDinamico.add(12)
+    print(arregloDinamico)
+
+    //FOR EACH=>Unit
+    //itera sobre cada elemento del arreglo
+    val respuestaForEach:Unit=arregloDinamico
+        .forEach{ valorActual:Int->
+            println("Valor actual: ${valorActual}");
+        }
+
+    //it, eso es el elemento iterado
+    arregloDinamico.forEach{ println("Valor actual: ${it}")}
+
+    //MAP->MUTA el arreglo
+    //1) Enviemos el nuevo valor de la iteracion
+    //2) Nos devuelve un NUEVO arreglo con los valores modificados
+    val respuestaMap:List<Double> = arregloDinamico
+        .map{valorActual:Int->
+            return@map valorActual.toDouble()+100.00 //map es el nombre del operador
+        }
+    println(respuestaMap)
+    val respuestaMapDos=arregloDinamico.map { it+15 }
+    println(respuestaMapDos)
+
+    //FILTER->FILTRA el arreglo
+    //1) Enviemos una expresion (TRUE o FALSE)
+    //2) NUEVO arreglo con los valores filtrados
+    val respuestaFilter:List<Int> = arregloDinamico
+        .filter{valorActual:Int->
+            //expresion o condicion
+            val mayoresACinco:Boolean=valorActual>5
+            return@filter mayoresACinco
+        }
+
+    val respuestaFilterDos=arregloDinamico.filter { it<=5 }
+    println(respuestaFilter)
+    println(respuestaFilterDos)
+
+    //ANY->OR (alguna condicion se cumple?)
+    //ALL->AND (todas las condiciones se cumplen?)
+    val respuestaAny:Boolean=arregloDinamico
+        .any{valorActual:Int->
+            return@any valorActual>5
+        }
+    println(respuestaAny)//true
+    val respuestaAll:Boolean=arregloDinamico
+        .all{valorActual:Int->
+            return@all valorActual>5
+        }
+    println(respuestaAll)//false
+
+    //REDUCE->valor acumulado
+    //valorAcumulado=0 siempre empieza en 0 en kotlin
+    //[1,2,3,4,5]->acumular SUMAR estos valores del arreglo
+    //valor iterado=ValorEmpiezo +1=0+1=1->iteracion 1
+    //valor iterado=ValorAcumuladoIteracion1+2=1+2=3->iteracion 2
+    //valor iterado=ValorAcumuladoIteracion2+3=3+3=6->iteracion 3
+    //valor iterado=ValorAcumuladoIteracion3+4=6+4=10->iteracion 4
+    //valor iterado=ValorAcumuladoIteracion4+5=10+5=15->iteracion 5
+    val respuestaReduce:Int=arregloDinamico
+        .reduce{acumulado:Int, valorActual:Int->
+            return@reduce acumulado+valorActual//->cambiar o usar la logica de negocio
+        }
+    println(respuestaReduce)
+    //return@reduce acumulado+(itemCarrito.cantidad*itemCarrito.precio)
 }
 
-fun imprimirNombre(nombre:String):Unit{//no existe el void pero si el Unit, es la funcion que no devueove nada
+fun imprimirNombre(nombre:String):Unit{//no existe el void pero si el Unit, es la funcion que no devuelve nada
     fun otraFuncionAdentro(){ //funcion dentro de otra funcion
         println("Otra funcion adentro")//no es requerido poner unit
         //kotlin entiende que no devuelve nada
@@ -90,9 +177,9 @@ abstract class NumerosJava{
     }
 }
 abstract class Numeros(//constructor primario ojo-parentesis() para cosntructor primario
-    protected val numeroUno:Int//instancia.numeroUno
-    private val numeroDos:Int//instancia.numeroDos
-    parametroNoUsadoNoPropiedadDeLaClase:Int?=null
+    protected val numeroUno:Int,//instancia.numeroUno
+    protected val numeroDos:Int//instancia.numeroDos //protected
+    //parametroNoUsadoNoPropiedadDeLaClase:Int?=null
 ){
         init {//BLOQUE DE CODIGO DEL CONSTRUCTOR PRIMARIO
         this.numeroUno
@@ -108,4 +195,76 @@ class Suma(
     unoParametro,
     dosParametro
 ){
+    public val soyPublicoExplicito:String ="Publicar"
+    val soyPublicoImplicito="Publico implicito"//el modificador de acceso es public por defecto
+    init{//bloque constructor primario
+        this.numeroUno
+        this.numeroDos
+        numeroUno //this es opcional (propiedades, metodos)
+        numeroDos
+        this.soyPublicoImplicito
+        soyPublicoExplicito
+    }
+    constructor(//constructor secundario
+        uno:Int?, //entero nulable
+        dos:Int,
+    ):this(
+        if (uno==null) 0 else uno,
+        dos
+    ){
+        //bloque de codigo del constructor secundario
+
+    }
+
+    constructor(//constructor secundario
+        uno:Int, //entero nulable
+        dos:Int?,
+    ):this(
+        uno,
+        if (dos==null) 0 else dos
+    ){
+
+    }
+
+    constructor(//constructor secundario
+        uno:Int?, //entero nulable
+        dos:Int?, //entero nulable
+    ):this(
+        if (uno==null) 0 else uno,
+        if (dos==null) 0 else dos
+    ){
+
+    }
+
+    fun sumar():Int{
+        val total=numeroUno+numeroDos
+        agregarHistorial(total)
+        return total
+    }
+    //no hay propiedades ni metodos estaticos en kotlin.
+    //companion object, es un objeto que acompaña a todas las instancias de la clase
+    companion object{ //comparte entre todas las instancias, similar al static de java
+        //funciones y variables
+        //es accesible NombreClase.metodo, NombreClase.funcion=>
+        //Suma.pi
+        val pi=3.1416
+        //Suma.elevadoAlCuadrado
+        fun elevarAlCuadrado(num:Int):Int{ return num*num }
+        val historialSumas= arrayListOf<Int>()
+        fun agregarHistorial(valorTotalSuma:Int) {//Suma.agregarHistorial
+            historialSumas.add(valorTotalSuma)
+        }
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
